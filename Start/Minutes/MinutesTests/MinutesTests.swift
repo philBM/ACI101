0c30b202bce27c15b1374063f7082834ef1d84f9
philBM <32509443+philBM@users.noreply.github.com>
2022-03-18 00:24:56 +0000

    first unit test
    testSerialization

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,36 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by BiomedicalMac on 17/03/2022.
+//  Copyright © 2022 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+
+    var entryUnderTest: Entry!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        entryUnderTest = Entry("Title", "Content")
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        entryUnderTest = nil
+        super.tearDown()
+    }
+
+    func testSerialization() {
+            let serializedData = FileEntryStore.serialize(entryUnderTest)
+            let entry = FileEntryStore.deserialize(serializedData!)
+            
+            XCTAssertEqual(entry?.title, "Titled", "Title does not match")
+            XCTAssertEqual(entry?.content, "Content", "Content does not match")
+        }
+
+}
