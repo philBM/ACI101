c3aed0e503ac92ded7dae8fedcb4c724d46b50fd
philBM <32509443+philBM@users.noreply.github.com>
2022-03-20 20:06:23 +0000

    build for 9
    app centrer added

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,8 @@
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -6,9 +10,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     static var entries: FileEntryStore = FileEntryStore()
     
     var window: UIWindow?
-
+//4bfa7c2b-79db-4809-baf1-25c9d5073876
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        
+        AppCenter.start(withAppSecret: "4bfa7c2b-79db-4809-baf1-25c9d5073876", services: [Crashes.self,Analytics.self, Distribute.self])
         return true
     }
 
